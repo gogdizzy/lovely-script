fdda24cefb5041d236f96aec39283ddae56ed655
gogdizzy <gogdizzy@gmail.com>
2023-03-24 12:22:02 +0800

    mod files

--- for-zz/empty-to-null.sh
+++ for-zz/empty-to-null.sh
@@ -1,2 +1,9 @@
 
-awk -F',' 'NR==1{ cols = NF; OFS=","; } NR!=1{ for( i = 1; i <= cols; ++i ) if($i=="") $i="NULL"; }1' ${1}
+awk -F',' '
+NR==1{ cols = NF; OFS=","; $1=$1; }
+NR!=1{ 
+  for( i = 1; i <= cols; ++i )
+  {
+    if($i=="") $i="NA";
+  }
+}1' "${1}"
